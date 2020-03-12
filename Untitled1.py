f79e4efe3c6b17849a647448cd3b8cb2ccb41103
dorjee515 <pratapdorjee515@gmail.com>
2020-03-13 03:28:15 +0530

    program 2

---
+++ Untitled1.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# # python programming
+# 
+
+# In[2]:
+
+
+print("hello")
+
+
+# In[13]:
+
+
+prog_lang=["python","c++","c","r","java"]
+print(type(prog_lang))
+prog_lang.sort()
+for i in prog_lang:
+    print(i)
+
